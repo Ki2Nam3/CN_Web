62148e615f28ba9a58cc582e923b386e435abee2
ĐạtĐQ <dat1999vn@gmail.com>
2020-04-25 00:45:34 -0700

    Đạt push
    base web 25/4/2020

---
+++ HotelBooking/HotelBooking/Controllers/homeController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelBooking.Controllers
+{
+    public class homeController : Controller
+    {
+        //
+        // GET: /home/
+
+        //------------------giao dien cua Dang Quang Dat
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult DichVu()
+        {
+            return View();
+        }
+
+        public ActionResult dvNhaHang()
+        {
+            return View();
+        }
+
+        public ActionResult dvHoBoi()
+        {
+            return View();
+        }
+
+        public ActionResult dvSpa()
+        {
+            return View();
+        }
+
+        public ActionResult dvGym()
+        {
+            return View();
+        }
+        //---------------------------------------------
+
+        //
+        // GET: /home/Details/5
+
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        //
+        // GET: /home/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /home/Create
+
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /home/Edit/5
+
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /home/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        //
+        // GET: /home/Delete/5
+
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        //
+        // POST: /home/Delete/5
+
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
